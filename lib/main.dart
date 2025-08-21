eeb9815123afa5fac88f3980e8f82f34ab73ec18
dejanazul <dejanazoel@gmail.com>
2025-08-21 22:58:14 +0700

    commit 2.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lunaria/screens/calendar/calendar_screen.dart';
+import 'package:lunaria/authentication/login.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +11,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: CalendarScreen(),
+      home: LoginScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
